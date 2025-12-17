18c756262325b2da1f93fdc612373032422b9639
榴弹 <82273342+liudanT800@users.noreply.github.com>
2025-12-17 18:12:22 +0800

    修复

--- tools/plotter.cpp
+++ tools/plotter.cpp
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#include "plotter.hpp"
-
-#include <arpa/inet.h>   // htons, inet_addr
-#include <sys/socket.h>  // socket, sendto
-#include <unistd.h>      // close
-
-namespace tools
-{
-Plotter::Plotter(std::string host, uint16_t port)
-{
-  socket_ = ::socket(AF_INET, SOCK_DGRAM, 0);
-
-  destination_.sin_family = AF_INET;
-  destination_.sin_port = ::htons(port);
-  destination_.sin_addr.s_addr = ::inet_addr(host.c_str());
-}
-
-Plotter::~Plotter() { ::close(socket_); }
-
-void Plotter::plot(const nlohmann::json & json)
-{
-  std::lock_guard<std::mutex> lock(mutex_);
-  auto data = json.dump();
-  ::sendto(
-    socket_, data.c_str(), data.length(), 0, reinterpret_cast<sockaddr *>(&destination_),
-    sizeof(destination_));
-}
-
-=======
 #include "plotter.hpp"
 
 #include <arpa/inet.h>   // htons, inet_addr
@@ -67,5 +37,4 @@ void Plotter::plot(const nlohmann::json & json)
   // }
 }
 
->>>>>>> 2e6e333 (测试)
 }  // namespace tools
